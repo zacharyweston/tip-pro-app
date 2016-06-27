cb6d3f64e199778b00530397cf4b12e869f694bd
Zachary Weston <zakweston@zacharys-mbp.mhc.local>
2016-06-27 12:45:12 -0400

    Next push

--- www/js/app.js
+++ www/js/app.js
@@ -6,8 +6,13 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 
-// Zak - Added 'firebase' here
-angular.module('app', ['ionic','firebase', 'ionic.service.core', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
+angular.module('app', ['ionic','ionic.service.core', 'app.controllers',
+  'app.routes',
+  'app.services',
+  'app.directives',
+  'auth0',
+  'angular-storage',
+  'angular-jwt'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,3 +28,37 @@ angular.module('app', ['ionic','firebase', 'ionic.service.core', 'app.controller
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider, authProvider, $httpProvider,
+  jwtInterceptorProvider) {
+
+  $stateProvider
+  // This is the state where you'll show the login
+  .state('login', { // Notice: this state name matches the loginState property value to set in authProvider.init({...}) below...
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl',
+  })
+  // Your app states
+  .state('userInfo', {
+    url: '/userInfo',
+    templateUrl: 'templates/user-info.html',
+    controller: 'UserInfoCtrl',
+    data: {
+      // This tells Auth0 that this state requires the user to be logged in.
+      // If the user isn't logged in and he tries to access this state
+      // he'll be redirected to the login page
+      requiresLogin: true
+    }
+  })
+  authProvider.init({
+    domain: 'tippro.auth0.com',
+    clientID: 'sAYgBnmp6fvnJjNXBfhaxVxIrzuPiR0v',
+    loginState: 'login' // This is the name of the state where you'll show the login, which is defined above...
+  });
+
+})
+.run(function(auth) {
+  // This hooks all auth events to check everything as soon as the app starts
+  auth.hookEvents();
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,40 @@
 angular.module('app.controllers', [])
-  
+
 .controller('homeCtrl', function($scope) {
 
 })
-   
+
 .controller('tipConfirmationCtrl', function($scope) {
 
 })
-   
+
 .controller('createAnAccountCtrl', function($scope) {
 
 })
-      
-.controller('loginCtrl', function($scope) {
 
+.controller('loginCtrl', function(store, $scope, $location, auth) {
+  $scope.login = function() {
+    auth.signin({
+      authParams: {
+        scope: 'openid offline_access',
+        device: 'Mobile device'
+      }
+    }, function(profile, token, accessToken, state, refreshToken) {
+      // Success callback
+      store.set('profile', profile);
+      store.set('token', token);
+      store.set('refreshToken', refreshToken);
+      $location.path('/');
+    }, function() {
+      // Error callback
+    });
+  }
 })
-   
+
 .controller('helpCtrl', function($scope) {
 
 })
-   
+
 .controller('accountCtrl', function($scope) {
 
 })
- 
